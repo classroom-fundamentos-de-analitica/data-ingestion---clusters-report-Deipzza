7419c598d73e6c0b2960dc97b5db149d78d46e64
David Pérez <dperezz@unal.edu.co>
2022-10-29 18:33:45 -0500

    First try

--- pregunta.py
+++ pregunta.py
@@ -17,7 +17,33 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
-    df = pd.read_fwf('clusters_report.txt')
+    indice = -1
+
+    df = pd.read_fwf('clusters_report.txt', skiprows=4, 
+                    names = ['Cluster', 
+                            'Cantidad_de_palabras_clave', 
+                            'Porcentaje_de_palabras_clave', 
+                            'Principales_palabras_claves'], 
+                    widths = [9,16,16,77])
+
+    line = '' # variable para almacenar el texto a añadir
+
+    for index, row in df.iterrows():
+        
+        if index == 0 or not pd.isna(row['Cluster']):
+            indice = index # guardar el índice de la fila en donde se añadirán las demás
+        else:
+            # si no hay espacio antes, lo añade
+            if df.iloc[index-1, 3][-1] != ' ':
+                line += ' '
+            line += re.sub('\s{2,}', ' ', row['Principales_palabras_claves']) # usando regex reemplazar los más de dos espacios por un solo espacio entre palabras
+            df.iloc[indice, 3] += line
+            line = ''
+
+    # eliminar las filas vacías, reiniciar los índices y eliminar la columna índice resultante
+    df.dropna(axis = 0, inplace = True)
+    df.reset_index(inplace=True)
+    df.drop(axis=1, labels='index', inplace=True)
 
     return df
 
